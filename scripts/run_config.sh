a0241a2ca8ff13f3e75736968a1900c5aa6de020
vogulcan <vogulcan@sabanciuniv.edu>
2026-05-27 11:23:49 +0300

    update

--- scripts/run_config.sh
+++ scripts/run_config.sh
@@ -1,12 +1,12 @@
 config=/home/ogulcan/polychrom/configs/degron/control.yaml
-run_name=control_v2
+run_name=control_v3
 mkdir -p "runs/${run_name}"
 PYTHONPATH=. micromamba run -n polychrom \
   python -m polychrom.pipelines.loop_extrusion.cli all "${config}" \
   > "runs/${run_name}/pipeline_run.log" 2>&1
 
 config=/home/ogulcan/polychrom/configs/degron/degron.yaml
-run_name=degron_v2
+run_name=degron_v3
 mkdir -p "runs/${run_name}"
 PYTHONPATH=. micromamba run -n polychrom \
   python -m polychrom.pipelines.loop_extrusion.cli all "${config}" \
