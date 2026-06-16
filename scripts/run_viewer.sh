04b4f6044704c34c5a20ad5bb3947662d943345c
vogulcan <vogulcan@sabanciuniv.edu>
2026-06-16 23:10:05 +0300

    update

--- scripts/run_viewer.sh
+++
@@ -1,7 +0,0 @@
-PYTHONPATH=. MAMBA_ROOT_PREFIX=/home/carlos/micromamba micromamba run -n openmm \
-  python -m polychrom.pipelines.loop_extrusion.cli viewer \
-  configs/degron/degron.yaml
-
-PYTHONPATH=. MAMBA_ROOT_PREFIX=/home/carlos/micromamba micromamba run -n openmm \
-  python -m polychrom.pipelines.loop_extrusion.cli viewer \
-  configs/biophysical_calibrated.yaml runs/biophysical_calibrated
